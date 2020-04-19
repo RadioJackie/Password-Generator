728c1749d060f85006b5f11b6589c25726146956
RadioJackie <jacobpeters628@gmail.com>
2020-04-18 21:49:38 -0700

    Cleaned up the js and got rid of useless code

--- Homework-03/script.js
+++ Homework-03/script.js
@@ -3,47 +3,29 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-	console.log(1);
+	//console.log(1);
 	var password = generatePassword();
 	var passwordText = document.querySelector("#password");
 
 	passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); {
 
 	function generatePassword() {
+
 		let generatedPassword = '';
 		const passwordOptions = getUserInputs();
 		console.log(passwordOptions);
-		
-			var length = 24,
-				charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-				retVal = "";
-			for (var i = 0, n = charset.length; i < length; ++i) {
-				retVal += charset.charAt(Math.floor(Math.random() * n));
-			}
-			return retVal;
-		
-		return generatedPassword
-	}
-	function getRandomLower() {
-		return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-	}
 
-	function getRandomUpper() {
-		return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-	}
-
-	function getRandomNumber() {
-		return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-	}
-
-	function getRandomSymbol() {
-		const symbols = '!@#$%^&*(){}[]=<>/,.'
-		return symbols[Math.floor(Math.random() * symbols.length)];
+		var length = 24,
+			charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*<>:,";
+		retVal = "";
+		for (var i = 0, n = charset.length; i < length; ++i) {
+			retVal += charset.charAt(Math.floor(Math.random() * n));
+		}
+		return retVal;
 	}
 
 	function getUserInputs() {
@@ -51,47 +33,15 @@ generateBtn.addEventListener("click", writePassword); {
 			numberOfChars: 0
 		};
 		userChoices.numberOfChars = parseInt(prompt('How many characters would you like?'));
-		userChoices.Lower = confirm("Would you like lower case charcters?")
-		userChoices.Upper = confirm("Would you like Upper case charcters?")
-		userChoices.Number = confirm("Would you like Numbers?")
-		userChoices.Symbols = confirm("Would you like Symbols?")
+		userChoices.Lower = confirm("Would you like lower case charcters?");
+		userChoices.Upper = confirm("Would you like Upper case charcters?");
+		userChoices.Number = confirm("Would you like Numbers?");
+		userChoices.Symbols = confirm("Would you like Symbols?");
 
 		return userChoices;
 	}
-}
 
-const passwordEl = document.getElementById('password')
-const generateEl = document.getElementById('generate')
-
-const randomFunc = {
-	lower: getRandomLower,
-	upper: getRandomUpper,
-	number: getRandomNumber,
-	symbol: getRandomSymbol
-}
-function password(lower, upper, number, symbol, length) {
-	let generatedPassword = '';
-	const typesCount = lower + upper + number + symbol;
-	const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
-}
-
-	function getRandomLower() {
-		return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-	}
-
-	function getRandomUpper() {
-		return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-	}
-
-	function getRandomNumber() {
-		return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-	}
-
-	function getRandomSymbol() {
-		const symbols = '!@#$%^&*(){}[]=<>/,.'
-		return symbols[Math.floor(Math.random() * symbols.length)];
-	}
-
+	
 
 /*
 Psudo code
@@ -100,52 +50,3 @@ Psudo code
 3.)Choose a length betweem 8 and 128 characters for your password generator
 4.)
 */
-
-
-/*
-// Dom elements
-const passwordEl = document.getElementById('password')
-const generateEl = document.getElementById('generate')
-const lengthEl = document.getElementById('length')
-
-
-
-
-
-const randomFunc = {
-	lower: getRandomLower,
-	upper: getRandomUpper,
-	number: getRandomNumber,
-	symbol: getRandomSymbol
-}
-
-
-);
-
-
-
-
-// Generator Functions
-
-function password(lower, upper, number, symbol, length) {
-	let generatedPassword = '';
-	const typesCount = lower + upper + number + symbol;
-	const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
-
-	function getRandomLower() {
-		return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-	}
-
-	function getRandomUpper() {
-		return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-	}
-
-	function getRandomNumber() {
-		return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-	}
-
-	function getRandomSymbol() {
-		const symbols = '!@#$%^&*(){}[]=<>/,.'
-		return symbols[Math.floor(Math.random() * symbols.length)];
-	}
-	*/
