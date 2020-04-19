d0777ae63a7680dcf514907540f16d078e2cff86
RadioJackie <jacobpeters628@gmail.com>
2020-04-18 20:49:13 -0700

    IT WORKS!!!! The code finally displays!!!! Made a for loop.

--- Homework-03/script.js
+++ Homework-03/script.js
@@ -12,43 +12,86 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); {
 
-function generatePassword() {
+	function generatePassword() {
+		let generatedPassword = '';
+		const passwordOptions = getUserInputs();
+		console.log(passwordOptions);
+		
+			var length = 24,
+				charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+				retVal = "";
+			for (var i = 0, n = charset.length; i < length; ++i) {
+				retVal += charset.charAt(Math.floor(Math.random() * n));
+			}
+			return retVal;
+		
+		return generatedPassword
+	}
+	function getRandomLower() {
+		return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+	}
+
+	function getRandomUpper() {
+		return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+	}
+
+	function getRandomNumber() {
+		return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+	}
+
+	function getRandomSymbol() {
+		const symbols = '!@#$%^&*(){}[]=<>/,.'
+		return symbols[Math.floor(Math.random() * symbols.length)];
+	}
+
+	function getUserInputs() {
+		let userChoices = {
+			numberOfChars: 0
+		};
+		userChoices.numberOfChars = parseInt(prompt('How many characters would you like?'));
+		userChoices.Lower = confirm("Would you like lower case charcters?")
+		userChoices.Upper = confirm("Would you like Upper case charcters?")
+		userChoices.Number = confirm("Would you like Numbers?")
+		userChoices.Symbols = confirm("Would you like Symbols?")
+
+		return userChoices;
+	}
+}
+
+const passwordEl = document.getElementById('password')
+const generateEl = document.getElementById('generate')
+
+const randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol
+}
+function password(lower, upper, number, symbol, length) {
 	let generatedPassword = '';
-	const passwordOptions = getUserInputs();
-	console.log(passwordOptions);
-	return generatedPassword
-}
-function getRandomLower() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+	const typesCount = lower + upper + number + symbol;
+	const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
 }
 
-function getRandomUpper() {
-	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+	function getRandomLower() {
+		return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+	}
 
-function getRandomNumber() {
-	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+	function getRandomUpper() {
+		return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+	}
 
-function getRandomSymbol() {
-	const symbols = '!@#$%^&*(){}[]=<>/,.'
-	return symbols[Math.floor(Math.random() * symbols.length)];
-}
+	function getRandomNumber() {
+		return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+	}
 
-function getUserInputs() {
-	let userChoices = {
-		numberOfChars: 0
-	};
-	userChoices.numberOfChars = parseInt(prompt('How many characters would you like?'));
-	userChoices.Lower = confirm("Would you like lower case charcters?")
-	userChoices.Upper = confirm("Would you like Upper case charcters?")
-	userChoices.Number = confirm("Would you like Numbers?")
-	userChoices.Symbols = confirm("Would you like Symbols?")
+	function getRandomSymbol() {
+		const symbols = '!@#$%^&*(){}[]=<>/,.'
+		return symbols[Math.floor(Math.random() * symbols.length)];
+	}
 
-	return userChoices;
-}
 
 /*
 Psudo code
